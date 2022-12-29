86de4f120dd0318919d8689e6086b150d20bbdcd
Crowdbotics <team@crowdbotics.com>
2022-12-29 18:31:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/meal_planner_templa_38066/MainActivity.java
@@ -0,0 +1,15 @@
+package com.meal_planner_templa_38066;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "meal_planner_templa_38066";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "meal_planner_templa_38066",
+  url: "https://meal_planner_templa_38066.botics.co",
+  api: "https://meal_planner_templa_38066.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
